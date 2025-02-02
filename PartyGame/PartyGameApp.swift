a156ff5fa52755e9a2346c85a7e57f079ebfe6d3
Lohann Excoffon <lohann.excoffon@gmail.com>
2025-02-02 14:23:59 -0600

    Initial Commit

---
+++ PartyGame/PartyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyGameApp.swift
+//  PartyGame
+//
+//  Created by Lohann Excoffon on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PartyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
