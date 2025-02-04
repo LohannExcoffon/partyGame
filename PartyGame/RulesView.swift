9c357e53ec60f6e48ca7fede27fc465591d8c166
Lohann Excoffon <lohann.excoffon@gmail.com>
2025-02-04 15:31:57 -0600

    RulesView

--- PartyGame/RulesView.swift
+++ PartyGame/RulesView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct RulesView: View {
+    
+    let imageNames = ["pointingIcon", "fingerRaisedIcon"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [.blue, .red, .purple]), startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea(edges: .all)
+                
+                List(imageNames, id: \.self) { imageName in
+                    HStack {
+                        Image(imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50) // Thumbnail size
+
+                        Text(imageName)
+                            .font(.headline)
+                        
+                    } .padding(5)
+                        .listRowBackground(Color.clear)
+                } .scrollContentBackground(.hidden)
+            }.navigationTitle("Game Rules")
+            
+            
+        }
     }
 }
 
