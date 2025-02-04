b4f04c4d5ac971c21ce806caffb4b66dda3991c3
Lohann Excoffon <lohann.excoffon@gmail.com>
2025-02-04 14:40:28 -0600

    button on JoinGameView

--- PartyGame/JoinGameView.swift
+++ PartyGame/JoinGameView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct JoinGameView: View {
+    
+    @State private var userInput: String = ""
+    
     var body: some View {
+        
         
         ZStack {
             // Colorful background
@@ -16,6 +20,37 @@ struct JoinGameView: View {
                 .ignoresSafeArea(edges: .all)
             
             VStack {
+                Text("Enter Game Pin:")
+                    .font(.custom("Courier New", size: 30))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    Spacer()
+                    
+                    TextField("Game Pin...", text: $userInput)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.trailing, 10)
+                    
+                    
+                    Button(action: {
+                        print("User typed: \(userInput)")
+                        userInput = ""
+                    }) {
+                        Text("Join Game")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .frame(height: 50.0)
+                            .background(Color.white)
+                            .foregroundColor(.blue)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                    }
+                    .frame(height: 70.0)
+                    
+                    Spacer()
+                }
+                .frame(width: 300.0, height: 70)
                 
             }
 
