7f3dc889a487f56d4d359dbfafe5229db73b2b42
Lohann Excoffon <lohann.excoffon@gmail.com>
2025-02-07 21:58:57 -0600

    button on HomeView to RulesView

--- PartyGame/ContentView.swift
+++ PartyGame/ContentView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image(systemName: "star.fill")
+        NavigationStack {
+                    VStack {
+                        Text("Welcome to the Main View")
+                            .font(.title)
+                            .padding()
+                        
+                        // Navigation Button to go to AnotherView
+                        NavigationLink(destination: RulesView()) {
+                            Text("Go to Next View")
+                                .font(.headline)
+                                .padding()
+                                .frame(width: 200)
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }
+                }
     }
 }
 
--- PartyGame/HomeView.swift
+++ PartyGame/HomeView.swift
@@ -9,74 +9,91 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-                    // Colorful background w/ gradient
-            LinearGradient(gradient: Gradient(colors: [.blue, .red, .purple]), startPoint: .top, endPoint: .bottom)
-                        .edgesIgnoringSafeArea(.all)
-                    
-            VStack {
-                // Game Title
+        NavigationStack {
+            ZStack {
+                // Colorful background w/ gradient
+                LinearGradient(gradient: Gradient(colors: [.blue, .red, .purple]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
                 VStack {
-                    Text("PARTY")
-                        .font(.custom("Courier New", size: 50))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .shadow(radius: 5)
-                    Text("GAME")
-                        .font(.custom("Courier New", size: 50))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .shadow(radius: 5)
-                } .padding(.top, 100)
-                
-                Spacer()
-                
-                // Buttons
-                VStack(spacing: 20) {
-                    Button(action: {
-                        // Action for joining a game
-                    }) {
-                        Text("Join Game")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .frame(width: 200, height: 50)
-                            .background(Color.white)
-                            .foregroundColor(.blue)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
+                    // button to see rules
+                    HStack {
+                        Spacer()
+                        NavigationLink(destination: RulesView()) {
+                            Image(systemName: "info.circle.fill")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+                                .background(Color.black.opacity(0))
+                                .clipShape(Circle())
+                                .shadow(radius: 5)
+                                .padding(.trailing, 20)
+                        }
                     }
                     
-                    Button(action: {
-                        // Action for creating a game
-                    }) {
-                        Text("Create Game")
-                            .font(.title2)
-                            .fontWeight(.semibold)
-                            .frame(width: 200, height: 50)
-                            .background(Color.white)
-                            .foregroundColor(.purple)
-                            .cornerRadius(10)
+                    // Game Title
+                    VStack {
+                        Text("PARTY")
+                            .font(.custom("Courier New", size: 50))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
                             .shadow(radius: 5)
+                        Text("GAME")
+                            .font(.custom("Courier New", size: 50))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .shadow(radius: 5)
+                    } .padding(.top, 80)
+                    
+                    Spacer()
+                    
+                    // Buttons
+                    VStack(spacing: 20) {
+                        Button(action: {
+                            // Action for joining a game
+                        }) {
+                            Text("Join Game")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .frame(width: 200, height: 50)
+                                .background(Color.white)
+                                .foregroundColor(.blue)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Button(action: {
+                            // Action for creating a game
+                        }) {
+                            Text("Create Game")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .frame(width: 200, height: 50)
+                                .background(Color.white)
+                                .foregroundColor(.purple)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
                     }
-                }
-                .padding(.bottom, 100)
-                
-                // HStack at the bottom with icons and text with a tinted background
-                HStack {
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.white)
+                    .padding(.bottom, 100)
                     
-                    Text("Placeholder")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                    
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.white)
+                    // HStack at the bottom with icons and text with a tinted background
+                    HStack {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.white)
+                        
+                        Text("Placeholder")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                        
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.white)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
                 }
-                .frame(maxWidth: .infinity)
-                .padding()
             }
-        }
+        } 
     }
 }
 
